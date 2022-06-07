f3417dbde78a5594fc246fffdd8d0e7082ece585
Niroj925 <ngtc0925@gmail.com>
2022-06-07 22:03:09 +0545

    ejs

--- ejs_tmplt_engine/index.js
+++ ejs_tmplt_engine/index.js
@@ -14,16 +14,24 @@ const __dirname=dirname(__filename);
 app.get('/', (req, res) => {
     //res.sendFile(__dirname+'/view/homepage.html');
     //render properties is used to convert our ejs file into html file 
-res.render('homepage');//ejs file must be in views folder
+res.render('homepage',{title:"Merohomepage"});//ejs file must be in views folder
 
 })
 app.get('/aboutus/', (req, res) => {
-   res.render('about',{title: 'About'});
+   res.render('about',{
+     title: 'About',
+     service:["app dev","web design","seo",'automation']
+    });
 })
 app.get('/services', (req, res) => {
-  res.render('service');
+  res.render('service',{title:"our serevces"});
 })
 
+//if no any page found then we can send a msg
+//that must at the bottom
+app.get("*",(req,res) => {
+  res.send('<b>Page Not Found</b>');
+})
 app.listen(8000,(req,res) => {
     console.log('server is running');
 
