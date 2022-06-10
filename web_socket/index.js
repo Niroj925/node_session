786c494c95cf35d499c1b8411430641439219644
Niroj925 <ngtc0925@gmail.com>
2022-06-10 21:13:52 +0545

    aded

--- web_socket/index.js
+++ web_socket/index.js
@@ -15,28 +15,30 @@ const server =http.createServer(app);
 const __filename=fileURLToPath(import.meta.url);
 const __dirname=dirname(__filename);
 //this make web socket
-//websocket has two side seperately cilent n server side
+//websocket has two version cilent n server side
 const io=new Server(server);//to make bidirectional connections this is also make in client too
 
 
 app.get('/',(req, res)=>{
-    res.sendFile(__dirname+"/index.html");
+    res.sendFile(__dirname+ "/index.html");
 })
 //to get real time information while connection and disconnection what have done
 //
 io.on('connection', (socket) => {
-    console.log('a user connected');
+    console.log('a user connected:'+socket.id);
 
     socket.on('disconnect', () => {
-      console.log('user disconnected');
+      console.log('user disconnected:'+socket.id);
     });
-socket.on('chat message',(msg)=> {
-  console.log("message: " + msg);
+    //it receive the input value from client event
+    //chat message is a input event name in client
+socket.on('chat message',(data)=> {
+ // console.log("message: " + data);
+//to broadcast input message to all client from server 
+  io.emit('chat message', data);
   });
-  });
-
   
-
-app.listen(8080,()=>{
+  });
+  server.listen(8080, () => {
     console.log('server is running');
-})
+  });
