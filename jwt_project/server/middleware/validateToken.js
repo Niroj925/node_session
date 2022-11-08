6514448434c951b95fbe73d588f540d06399bfeb
Niroj925 <ngtc0925@gmail.com>
2022-11-08 23:04:41 +0545

    validate user with jwt token from frontend and validate by backend jwt

--- jwt_project/server/middleware/validateToken.js
+++ jwt_project/server/middleware/validateToken.js
@@ -3,18 +3,22 @@ import jwt from 'jsonwebtoken';
 import "dotenv/config";
 
 const validity= (req, res, next) =>{
-    //lets take token from localstorage which we have been configured
+    //lets take token from localstorage of frontend which we have been configured
 
 const token=req.headers.token;
-try{
+console.log(token);
 if(token){
+    try{
     const isValid=jwt.verify(token,process.env.JWT_SECRET);
     if(isValid) next();
     else 
-    return res.status(403).json({success:false,message:"invalid token"});
-}else res.status(403).json('token not provided');
+     return res.status(403).json({success:false,message:"invalid token"});
 }catch(err){
     return res.status(403).json(err);
 };
+}
+else res.status(403).json('token not provided');
+
+
 }
 export default validity;
